375fbd4a895cc9fc589c1eabe71bb2074fd56464
Spencer Alan <spencer.alan@icloud.com>
2017-04-28 09:52:13 -0700

    add method for a new definition and method for a new word that holds multiple definition objects

--- lib/definer.rb
+++ lib/definer.rb
@@ -1,9 +1,35 @@
-# example ruby code
+class Definition
 
-# class Palindrome
+  attr_accessor :describe
 
-#   def is_word?(user_input)
-#     user_input.match?(/[aeiouy]+/i)
-#   end
+  def initialize(definition)
+    self.describe = definition
+  end
 
-# end
+end
+
+class Word
+  attr_accessor :describe
+  attr_accessor :definitions
+
+  def initialize(word)
+    self.describe = word
+    self.definitions = Array.new
+  end
+
+  def add_definition(definition)
+    #expecting a definition object. throw error if anything else.
+    self.definitions.push(definition)
+  end
+
+  def definition
+    definitions.map do |definition|
+      definition.describe
+    end.join(" | ")
+  end
+
+end
+
+module Dictionary
+
+end
--- spec/definer_spec.rb
+++ spec/definer_spec.rb
@@ -2,18 +2,50 @@ require "definer"
 require "rspec"
 require "pry"
 
-
-# example project spec
-
-# describe 'Palindrome#isWord?' do
-#   let(:word) { Palindrome.new }
-
-#   it "returns true if the word has at least one vowel" do
-#     expect(word.is_word?("word")).to eq true
-#   end
-
-#   it "returns false if the word does not have a vowel" do
-#     expect(word.is_word?("wrd")).to eq false
-#   end
-
+# describe "Dictionary" do
 # end
+
+describe "Word" do
+
+  describe "describe" do
+    it "returns the word" do
+      word = Word.new("apple")
+      expect(word.describe).to eq "apple"
+    end
+  end
+
+  describe "definition" do
+    word = Word.new("apple")
+
+    it "returns the definition" do
+      definition1 = Definition.new("fruit")
+      word.add_definition(definition1)
+      expect(word.definition).to eq "fruit"
+    end
+
+    it "returns multiple definitions" do
+      definition2 = Definition.new("edible")
+      word.add_definition(definition2)
+      expect(word.definition).to eq "fruit | edible"
+    end
+
+    it "returns long definitions" do
+      definition3 = Definition.new("falls from trees")
+      word.add_definition(definition3)
+      expect(word.definition).to eq "fruit | edible | falls from trees"
+    end
+  end
+
+end
+
+describe "Definition" do
+
+  let(:definition) { Definition.new("it does things") }
+
+  describe "describe" do
+    it "returns its definition" do
+      expect(definition.describe).to eq "it does things"
+    end
+  end
+  
+end
