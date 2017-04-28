a64c2f48e1e0402e5a19f9527791f8af8d03ce02
Spencer Alan <spencer.alan@icloud.com>
2017-04-28 16:53:44 -0700

    add integration specs for deleting a word and adding a new definition to the word

--- spec/definer_integration_spec.rb
+++ spec/definer_integration_spec.rb
@@ -4,7 +4,7 @@ require "./app"
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("the add word path", {:type => :feature}) do
+describe("the word path", {:type => :feature}) do
 
   after do
     Dictionary.empty
@@ -13,20 +13,44 @@ describe("the add word path", {:type => :feature}) do
   it("processes the user input and adds the word to the dictionary") do
     visit("/")
     click_link("add word")
-    fill_in("word", :with => "word")
-    fill_in("definition", :with => "definition")
+    fill_in("word", :with => "happy")
+    fill_in("definition", :with => "pants")
     click_button("add word")
-    expect(page).to have_content("word")
-    expect(page).to have_content("definition")
+    expect(page).to have_content("happy")
+    expect(page).to have_content("pants")
   end
 
-  it("processes the user input and adds the word to the dictionary") do
+  it("did not add the word to the dictionary if the user clicked the back button") do
     visit("/")
     click_link("add word")
-    fill_in("word", :with => "word")
-    fill_in("definition", :with => "definition")
+    fill_in("word", :with => "happy")
+    fill_in("definition", :with => "pants")
+    click_link("back")
+    expect(page).not_to have_content("happy")
+    expect(page).not_to have_content("pants")
+  end
+
+  it("processes the user input and deletes a word from the dictionary") do
+    visit("/")
+    click_link("add word")
+    fill_in("word", :with => "happy")
+    fill_in("definition", :with => "pants")
     click_button("add word")
-    expect(page).to have_content("word")
-    expect(page).to have_content("definition")
+    click_link("happy")
+    fill_in("definition", :with => "smarty")
+    click_button("add definition")
+    expect(page).to have_content("pants | smarty")
+  end
+
+  it("processes the user input and adds a second definition to the word") do
+    visit("/")
+    click_link("add word")
+    fill_in("word", :with => "happy")
+    fill_in("definition", :with => "pants")
+    click_button("add word")
+    click_link("happy")
+    click_link("delete word")
+    expect(page).not_to have_content("happy")
+    expect(page).not_to have_content("pants")
   end
 end
